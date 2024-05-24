4cfc1ea6a7cf9a56cc12b6a2c9af9bb953e4549d
Ilola Joao <ilolanicolau1999@gmail.com>
2024-05-24 10:26:06 +0100

    Add project files.

---
+++ RabbitMq.Reciever/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection=factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     autoDelete: false,
+                     exclusive: false,
+                     arguments: null);
+Console.WriteLine(" [*] Aguardando mensagens.");
+
+var consumer=new EventingBasicConsumer(channel);
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message=Encoding.UTF8.GetString(body);  
+
+    Console.WriteLine($" [x] Recebido: {message}"); 
+};
+
+channel.BasicConsume(queue:"hello", autoAck: true,consumer:consumer);
+
+Console.WriteLine("Aperte [enter] para sair.");
+Console.ReadLine();
+
+class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; } = string.Empty;
+}
---
+++ RabbitMq.Sender/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     autoDelete: false,
+                     exclusive: false,
+                     arguments: null);
+Console.WriteLine("Digite mensagem e aperte <ENTER>");
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(message))
+        break;
+
+    var aluno = new Aluno { Id = 1, Nome = "Ilola Nicolau João" };
+
+    message=JsonSerializer.Serialize(aluno);
+
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish(exchange: string.Empty, routingKey: "hello", basicProperties: null, body: body);
+
+    Console.WriteLine($" [x] Envio {message}");
+}
+
+
+class Aluno
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }=string.Empty;  
+}
